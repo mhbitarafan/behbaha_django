1897dd6c08b0f1a76e2141df49546b68e6e64d49
mhbitarafan <mhbitarafan1375@gmail.com>
2019-07-13 10:54:43 +0430

    22tir98

--- behbaha_django/settings.py
+++ behbaha_django/settings.py
@@ -118,20 +118,21 @@ WSGI_APPLICATION = 'behbaha_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 DATABASES = {
     'default': {
-        'NAME': 'basarfetar',
-        'ENGINE': 'django.db.backends.mysql',
-        'USER': 'root',
-        'PASSWORD': '123456'
-    },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
+
+# DATABASES = {
+#     'default': {
+#         'NAME': 'basarfetar',
+#         'ENGINE': 'django.db.backends.mysql',
+#         'USER': 'root',
+#         'PASSWORD': '123456'
+#     },
+# }
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
